f8100a18f648d28eb2ebe679fd5d37a84c394228
Renato <renatomacedo500@gmail.com>
2019-10-09 23:27:46 -0300

    scanner generates a output.json file and path to input file must be passed on args

--- scanner/src/index.js
+++ scanner/src/index.js
@@ -1,46 +1,57 @@
 const fs = require('fs');
 const path = require('path');
+const options = require('commander');
+try {
+  options
+    .version('0.0.1')
+    .option(
+      '-s --source <file_path>',
+      'Receives path of source code to compile.'
+    )
+    .parse(process.argv);
+  const lexems = require('./lexems');
+  const input = fs.readFileSync(options.source);
 
-const lexems = require('./lexems');
+  const inputString = input.toString();
+  let currentPosition = 0;
+  let programTokens = [];
+  let valueAccumulator = '';
+  let previousClass = '';
+  foundToken = false;
+  while (currentPosition < inputString.length) {
+    let currentChar = inputString[currentPosition];
 
-const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'));
-const inputString = input.toString();
-let currentPosition = 0;
-console.log(inputString.length);
-let programTokens = [];
-let valueAccumulator = '';
-let previousClass = '';
-foundToken = false;
-while (currentPosition < inputString.length) {
-  let currentChar = inputString[currentPosition];
-
-  if (foundToken) {
-    valueAccumulator = '';
-    foundToken = false;
-  }
-
-  valueAccumulator += currentChar;
-  for (let lexem of lexems) {
-    let pattern = lexem.pattern;
-    if (pattern.test(valueAccumulator)) {
-      // console.log(valueAccumulator);
-      if (lexem.class === 'number' && previousClass === 'Id') {
-        programTokens[programTokens.length - 1].value += currentChar;
-        previousClass = 'Id';
-      } else {
-        programTokens.push({ class: lexem.class, value: valueAccumulator });
-        previousClass = lexem.class;
-      }
-      foundToken = true;
-      break;
+    if (foundToken) {
+      valueAccumulator = '';
+      foundToken = false;
     }
-  }
-  if (!foundToken && previousClass === 'Id') {
-    programTokens[programTokens.length - 1].value += currentChar;
-    foundToken = true;
+
+    valueAccumulator += currentChar;
+    for (let lexem of lexems) {
+      let pattern = lexem.pattern;
+      if (pattern.test(valueAccumulator)) {
+        // console.log(valueAccumulator);
+        if (lexem.class === 'number' && previousClass === 'Id') {
+          programTokens[programTokens.length - 1].value += currentChar;
+          previousClass = 'Id';
+        } else {
+          programTokens.push({ class: lexem.class, value: valueAccumulator });
+          previousClass = lexem.class;
+        }
+        foundToken = true;
+        break;
+      }
+    }
+    if (!foundToken && previousClass === 'Id') {
+      programTokens[programTokens.length - 1].value += currentChar;
+      foundToken = true;
+    }
+
+    currentPosition++;
   }
 
-  currentPosition++;
+  fs.writeFileSync('output.json', JSON.stringify(programTokens));
+  console.log(programTokens);
+} catch (error) {
+  console.log(error.message);
 }
-
-console.log(programTokens);
--- scanner/src/lexems.js
+++ scanner/src/lexems.js
@@ -1,80 +1,79 @@
 const Lexems = [
   {
     pattern: /^while/,
-    class: 'while',
+    class: 'while'
   },
   {
     pattern: /^fn/,
-    class: 'funcao',
+    class: 'funcao'
   },
   {
     pattern: /^\(/,
-    class: 'abre_parentese',
+    class: 'abre_parentese'
   },
   {
     pattern: /^\)/,
-    class: 'fecha_parentese',
+    class: 'fecha_parentese'
   },
   {
     pattern: /^{/,
-    class: 'abre_chave',
+    class: 'abre_chave'
   },
   {
     pattern: /^}/,
-    class: 'fecha_chave',
+    class: 'fecha_chave'
   },
   {
     pattern: /^\d*\.?\d+/,
-    class: 'number',
+    class: 'number'
   },
   {
     pattern: /^int$|^float$|^string$|^boolean$|^void$/,
-    class: 'type',
+    class: 'type'
   },
   {
     pattern: /^(true|false)/,
-    class: 'true-false',
+    class: 'true-false'
   },
   {
     //pattern: /^\$[a-zA-Z]+\s$/,
     pattern: /^\$[_a-zA-Z]\w*/,
-    class: 'Id',
+    class: 'Id'
   },
 
   {
     pattern: /^=/,
-    class: 'assignment',
+    class: 'assignment'
   },
   {
     pattern: /^;/,
-    class: 'end',
+    class: 'end'
   },
   {
     pattern: /^\s$/,
-    class: 'whitespace',
+    class: 'whitespace'
   },
   {
     pattern: /^[\+|\-|\*|\/]/,
-    class: 'op_mat',
+    class: 'op_mat'
   },
   {
     pattern: /^&&|\^{1}|^and{1}|\|\|{1}|v{1}/,
-    class: 'op_Logico',
+    class: 'op_Logico'
   },
   {
     pattern: /^".*"$/,
-    class: 'string',
+    class: 'string'
   },
   {
     pattern: /^(==|>|>=|<|<=|=!)/,
-    class: 'comparadores',
+    class: 'comparadores'
   },
 
   {
     pattern: /^return/,
-    class: 'return',
-  },
+    class: 'return'
+  }
 ];
-const consta = 1;
-console.log(consta);
+
 module.exports = Lexems;
