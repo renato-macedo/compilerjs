f3ad0e716e472688b19bef2e1c20e0479dc096d1
Renato <spllrenato@gmail.com>
2019-10-10 14:52:37 -0300

    node index.js to run the scanner

---
+++ index.js
@@ -0,0 +1,3 @@
+const scanner = require('./scanner/src');
+
+scanner();
--- scanner/src/index.js
+++ scanner/src/index.js
@@ -1,57 +1,61 @@
 const fs = require('fs');
 const path = require('path');
 const options = require('commander');
-try {
-  options
-    .version('0.0.1')
-    .option(
-      '-s --source <file_path>',
-      'Receives path of source code to compile.'
-    )
-    .parse(process.argv);
-  const lexems = require('./lexems');
-  const input = fs.readFileSync(options.source);
 
-  const inputString = input.toString();
-  let currentPosition = 0;
-  let programTokens = [];
-  let valueAccumulator = '';
-  let previousClass = '';
-  foundToken = false;
-  while (currentPosition < inputString.length) {
-    let currentChar = inputString[currentPosition];
+module.exports = function () {
+  try {
+    // options
+    //   .version('0.0.1')
+    //   .option(
+    //     '-s --source <file_path>',
+    //     'Receives path of source code to compile.'
+    //   )
+    //   .parse(process.argv);
+    const lexems = require('./lexems');
+    const input = fs.readFileSync(path.resolve(__dirname, '..', '..', 'input.txt'));
 
-    if (foundToken) {
-      valueAccumulator = '';
-      foundToken = false;
-    }
+    const inputString = input.toString();
+    let currentPosition = 0;
+    let programTokens = [];
+    let valueAccumulator = '';
+    let previousClass = '';
+    foundToken = false;
+    while (currentPosition < inputString.length) {
+      let currentChar = inputString[currentPosition];
 
-    valueAccumulator += currentChar;
-    for (let lexem of lexems) {
-      let pattern = lexem.pattern;
-      if (pattern.test(valueAccumulator)) {
-        // console.log(valueAccumulator);
-        if (lexem.class === 'number' && previousClass === 'Id') {
-          programTokens[programTokens.length - 1].value += currentChar;
-          previousClass = 'Id';
-        } else {
-          programTokens.push({ class: lexem.class, value: valueAccumulator });
-          previousClass = lexem.class;
-        }
-        foundToken = true;
-        break;
+      if (foundToken) {
+        valueAccumulator = '';
+        foundToken = false;
       }
-    }
-    if (!foundToken && previousClass === 'Id') {
-      programTokens[programTokens.length - 1].value += currentChar;
-      foundToken = true;
+
+      valueAccumulator += currentChar;
+      for (let lexem of lexems) {
+        let pattern = lexem.pattern;
+        if (pattern.test(valueAccumulator)) {
+          // console.log(valueAccumulator);
+          if (lexem.class === 'number' && previousClass === 'Id') {
+            programTokens[programTokens.length - 1].value += currentChar;
+            previousClass = 'Id';
+          } else {
+            programTokens.push({ class: lexem.class, value: valueAccumulator });
+            previousClass = lexem.class;
+          }
+          foundToken = true;
+          break;
+        }
+      }
+      if (!foundToken && previousClass === 'Id') {
+        programTokens[programTokens.length - 1].value += currentChar;
+        foundToken = true;
+      }
+
+      currentPosition++;
     }
 
-    currentPosition++;
+    fs.writeFileSync('output.json', JSON.stringify(programTokens));
+    console.log(programTokens);
+  } catch (error) {
+    console.log(error.message);
   }
 
-  fs.writeFileSync('output.json', JSON.stringify(programTokens));
-  console.log(programTokens);
-} catch (error) {
-  console.log(error.message);
 }
